85ffaa6b72a27f4fc68cbc5d375bbc01268b8510
Ishnoor Kaur <119340690+IshnoorKaur@users.noreply.github.com>
2024-12-02 00:08:42 -0500

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,36 +1,54 @@
 //node --version # Should be >= 18 
 //npm install @google/generative-ai
 
-const {
+import {
     GoogleGenerativeAI,
     HarmCategory,
     HarmBlockThreshold,
-} = require("@google/generative-ai");
+} from "@google/generative-ai";
 
-const apiKey = "api";
-const genAI = new GoogleGenerativeAI(apiKey);
 
-const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-});
+async function run(prompt) {
+    const apiKey = "key";
+    const genAI = new GoogleGenerativeAI(apiKey);
 
-const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-};
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-pro",
+    });
 
-async function run() {
+    const generationConfig = {
+        temperature: 1,
+        topP: 0.95,
+        topK: 40,
+        maxOutputTokens: 8192,
+        responseMimeType: "text/plain",
+    };
+
+    const safetySettings = [{
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    }, {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    }, {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },];
+
     const chatSession = model.startChat({
         generationConfig,
         history: [
         ],
     });
 
-    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-    console.log(result.response.text());
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
 }
 
-run();
+
+export default run;
