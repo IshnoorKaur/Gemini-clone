7166151e715cd492f59ca5e8ffb4a757dedb7cf3
Ishnoor Kaur <119340690+IshnoorKaur@users.noreply.github.com>
2024-12-02 23:44:06 -0500

    Update Context.jsx

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -12,9 +12,59 @@ const ContextProvider = (props)=> {
     const [loading,setLoading] = useState(false); //if true display loading animation, after make it false
     const [resultData, setResultData] = useState(""); //siplay result on webpage
 
+    const delayPara = (index,nextWord) => {
+        setTimeout(function () {
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
+
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
 
     const onSent = async (prompt)=>{
-        await run(input)
+
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+
+        let response;
+        if(prompt !== undefined){
+            response = await run(prompt);
+            setRecentPrompt(prompt);
+        }
+        else{
+            setPreviousPrompts(prev=>[...prev,input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
+        //setRecentPrompt(input)
+        //setPreviousPrompts(prev=>[...prev,input])
+        //const response = await run(input) 
+
+        //replace double star with bold
+        let responseArray = response.split("**");
+        let newResponse="" ;
+        for(let i = 0; i<responseArray.length;i++){
+            if(i===0 || i%2 !== 1){ //even or zero
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>"+responseArray[i]+"</b>";
+            }
+        }
+
+        //replace single star with newline
+        let newResponse2 = newResponse.split("*").join("</br>");
+
+        let newResponseArray = newResponse2.split(" ");
+        for(let i=0; i<newResponseArray.length; i++){
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+" ");
+        }
+        setLoading(false)
+        setInput("")
+
     }
 
 
@@ -28,7 +78,8 @@ const ContextProvider = (props)=> {
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
         
     }
 
